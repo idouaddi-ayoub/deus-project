6e0e1cbf896e9f3d2592631eaa9fd361bbca8d95
idouaddi-ayoub <113615894+idouaddi-ayoub@users.noreply.github.com>
2023-09-26 01:57:05 +0100

    Tweaking in typescript

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,34 @@
+import DEUS from "../assets/images/DEUS.png";
+import { navlink } from "../constants";
+
 const Navbar = () => {
-  return <div>Bruh</div>;
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img
+            src={DEUS}
+            alt="DEUSGC"
+            width={150}
+            height={150}
+            className="m-0 w-[150px] h-[150px]"
+          />
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navlink.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
---
+++ src/constants/index.jsx
@@ -0,0 +1,6 @@
+export const navlink = [
+  { href: "#home", label: "Home" },
+  { href: "#about-us", label: "About Us" },
+  { href: "#games", label: "Games" },
+  { href: "#special-offer", label: "Special Offer" },
+];
